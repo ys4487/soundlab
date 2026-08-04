f310620585f1c56eb728357a009bd729c9087f82
ys4487 <ys0548574487@gmail.com>
2026-08-04 15:58:19 +0300

    Add files via upload

---
+++ gamma_brown.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.io.wavfile import write
+from scipy.signal import lfilter
+
+sample_rate = 44100
+duration = 1800
+
+t = np.linspace(0, duration, int(sample_rate * duration), False)
+
+# 1. תדרי גאמא (פער קיצוני של 40 הרץ)
+freq_left = 300.0
+freq_right = 340.0
+wave_left = np.sin(2 * np.pi * freq_left * t) * 0.4
+wave_right = np.sin(2 * np.pi * freq_right * t) * 0.4
+
+# 2. ייצור רעש חום
+white_noise = np.random.normal(0, 1, sample_rate * duration)
+brown_noise = lfilter([1], [1, -0.98], white_noise)
+brown_noise -= np.mean(brown_noise)
+brown_noise_normalized = (brown_noise / np.max(np.abs(brown_noise))) * 0.2
+
+# 3. חיבור
+left_channel = wave_left + brown_noise_normalized
+right_channel = wave_right + brown_noise_normalized
+
+stereo_signal = np.vstack((left_channel, right_channel)).T
+stereo_signal = np.int16((stereo_signal / np.max(np.abs(stereo_signal))) * 32767)
+
+write('gamma_brown.wav', sample_rate, stereo_signal)
+print("קובץ גאמא (טורבו) משולב עם רעש חום נוצר!")
